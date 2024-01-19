c1e8aad3ea11e019d2825f61daf2c2c7c345c489
mmattis <mattis.milochau@ynov.com>
2024-01-19 15:26:55 +0100

    app_test

--- init.sh
+++ init.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
+  GNU nano 5.6.1                                           init.sh                                                      #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
+
+
+if [[ $(id -u) -ne 0 ]] ; then
+  echo "Déso t pa rout"
+  exit 1
+fi
+
+# users
 groupadd backup
-useradd -m -d /srv/backup -g backup -s /usr/bin/nologin backup
+useradd -m -d /srv/backup -g backup -s /usr/sbin/nologin backup
+
+# log dir
 mkdir /var/log/backup/
 chmod 700 /var/log/backup/
 chown backup /var/log/backup/ -R backup
-chown backup.service
-chown backup backup.service
-chown backup backup.timer
-chmod 700 backup.service
-chmod 700 backup.timer
-mv backup.service /srv/backup/
-mv backup.timer /srv/backup/
+
+# systemd units
+chown backup backup.service backup.timer
+chmod 700 backup.service backup.timer
+mv backup.service backup.timer /etc/systemd/system/
+
 systemctl daemon-reload
-systemctl start backup.timer
-systemctl enable backup.timer
+systemctl enable --now backup.timer
