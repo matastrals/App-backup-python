04275d31d59fab3f3d855ffd9bdc466438ef3bff
mmattis <mattis.milochau@ynov.com>
2024-01-26 16:46:48 +0100

    fix

--- restore_backup.py
+++ restore_backup.py
@@ -1,9 +1,11 @@
 import tarfile
 import logging
 import json
+from backup import readJsonFile
 
 def restore(id: str):
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+    data_json = readJsonFile()
     try:
         with open('state.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
@@ -15,7 +17,7 @@ def restore(id: str):
         return
     try:
         with tarfile.open(path + id + "_" + time + '.tar', 'r') as file:
-            file.extractall(path)
+            file.extractall(data_json["backup"]["files"]["path"])
             logging.info("Restoring backup with id: " + id + "_" + time + " and path: " + data[id]['path'] + '/' + id + '.tar.gz')
     except Exception as e:
         logging.error("Error while extracting backup with id: " + id + " and path: " + data[id]['path'] + id + time + '.tar\nException:' + str(e))
